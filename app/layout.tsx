57a1e34971f95493f0ee585d58d0294d1feb9264
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2025-01-18 22:33:16 -0500

    Add script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" strategy="beforeInteractive" />
+      <Script src="https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.net.min.js" strategy="beforeInteractive" />
+      </head>
     <UserProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
